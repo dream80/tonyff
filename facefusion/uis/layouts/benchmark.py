8d0a057b9ccc4d9e2ebe530ebc68fc7f8929014b
dream80 <526381222@qq.com>
2023-11-06 20:41:45 +0800

    1.3.1

--- facefusion/uis/layouts/benchmark.py
+++ facefusion/uis/layouts/benchmark.py
@@ -1,22 +1,25 @@
 import gradio
 
-from facefusion.uis.components import about, processors, execution, execution_thread_count, execution_queue_count, limit_resources, benchmark_settings, benchmark
+import facefusion.globals
 from facefusion.utilities import conditional_download
+from facefusion.uis.components import about, frame_processors, frame_processors_options, execution, execution_thread_count, execution_queue_count, limit_resources, benchmark_options, benchmark
 
 
 def pre_check() -> bool:
-	conditional_download('.assets/examples',
-	[
-		'https://github.com/facefusion/facefusion-assets/releases/download/examples/source.jpg',
-		'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-240p.mp4',
-		'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-360p.mp4',
-		'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-540p.mp4',
-		'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-720p.mp4',
-		'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-1080p.mp4',
-		'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-1440p.mp4',
-		'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-2160p.mp4'
-	])
-	return True
+	if not facefusion.globals.skip_download:
+		conditional_download('.assets/examples',
+		[
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples/source.jpg',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-240p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-360p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-540p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-720p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-1080p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-1440p.mp4',
+			'https://github.com/facefusion/facefusion-assets/releases/download/examples/target-2160p.mp4'
+		])
+		return True
+	return False
 
 
 def pre_render() -> bool:
@@ -27,10 +30,11 @@ def render() -> gradio.Blocks:
 	with gradio.Blocks() as layout:
 		with gradio.Row():
 			with gradio.Column(scale = 2):
-				with gradio.Box():
+				with gradio.Blocks():
 					about.render()
 				with gradio.Blocks():
-					processors.render()
+					frame_processors.render()
+					frame_processors_options.render()
 				with gradio.Blocks():
 					execution.render()
 					execution_thread_count.render()
@@ -38,7 +42,7 @@ def render() -> gradio.Blocks:
 				with gradio.Blocks():
 					limit_resources.render()
 				with gradio.Blocks():
-					benchmark_settings.render()
+					benchmark_options.render()
 			with gradio.Column(scale= 5):
 				with gradio.Blocks():
 					benchmark.render()
@@ -46,15 +50,14 @@ def render() -> gradio.Blocks:
 
 
 def listen() -> None:
-	processors.listen()
+	frame_processors.listen()
+	frame_processors_options.listen()
 	execution.listen()
 	execution_thread_count.listen()
 	execution_queue_count.listen()
 	limit_resources.listen()
-	benchmark_settings.listen()
 	benchmark.listen()
 
 
 def run(ui : gradio.Blocks) -> None:
-	ui.queue(concurrency_count = 2, api_open = False)
-	ui.launch(show_api = False)
+	ui.queue(concurrency_count = 2, api_open = False).launch(show_api = False)
